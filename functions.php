ab6f3bcfc9b64311c22379eed9bb7984bbe963e2
Hoang <thaihoang88@gmail.com>
2023-09-03 02:08:54 +0700

    Allow upload SVG

--- functions.php
+++ functions.php
@@ -63,3 +63,22 @@ function wcboost_item_prop( $prop, $post_id = false ) {
 	return null;
 }
 
+// Allow SVG uploads.
+add_filter( 'upload_mimes', function( $mines ) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+} );
+
+add_action( 'admin_head', function() {
+	echo '
+	<style type="text/css">
+		.image-icon.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { width: 60px !important;  height: auto !important; }
+	</style>
+	';
+} );
+
+add_action( 'admin_init', function() {
+	if ( current_user_can( 'administrator' ) && ! defined( 'ALLOW_UNFILTERED_UPLOADS' ) ) {
+        define('ALLOW_UNFILTERED_UPLOADS', true);
+    }
+}, 1 );
