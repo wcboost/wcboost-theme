698fa6c037def00bbc833d549356b324e44889a7
Hoang <thaihoang88@gmail.com>
2023-12-28 11:35:10 +0700

    Add settings for the theme in Customizer

--- functions.php
+++ functions.php
@@ -7,7 +7,10 @@
  * Enqueue scripts
  */
 add_action( 'maart_after_enqueue_styles', function() {
-	wp_enqueue_style( 'wcboost', get_stylesheet_uri() );
+	$version = filemtime( get_stylesheet_directory() . '/style.css' );
+	$version = $version ?? wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'wcboost', get_stylesheet_uri(), null, $version );
 } );
 
 /**
@@ -25,6 +28,19 @@ add_action( 'init', function() {
 
 	$header->get_section( 'header-main' )->remove_component( 'search_icon', 'right', 20 );
 
+	$topbar = new \Maart\Structure\Base\Section( 'header-top' );
+	$topbar->priority = 5;
+	$topbar->add_component( [
+		'type'     => 'text',
+		'priority' => 10,
+		'column'   => 'center',
+		'data' => [
+			'content' => '🎉 Big Black Friday and Cyber Monday Sale! Save 40% on <a href="' . esc_url( get_post_type_archive_link( 'wcboost_plugin' ) ) . '">all plugins</a>! 🎉',
+		],
+	] );
+
+	// $header->add_section( $topbar );
+
 
 	// Modify footer.
 	$footer = $maart->frontend->get_component( 'footer' );
@@ -33,6 +49,16 @@ add_action( 'init', function() {
 
 	$footer->add_section( $footer_widgets );
 }, 20 );
+
+add_action( 'maart_component_section_text', function( $data ) {
+	$content = $data['content'] ?? '';
+
+	if ( $content ) {
+		?>
+		<div class="header-top-text"><?php echo wp_kses_post( $content ); ?></div>
+		<?php
+	}
+} );
 
 /**
  * Get item property from the post meta
@@ -99,6 +125,50 @@ add_filter ( 'woocommerce_add_to_cart_redirect', function() {
 
     return wc_get_checkout_url();
 }, 10, 2 );
+
+add_action( 'maart_customize_register', function( $manager ) {
+	$manager->add_section(
+		'wcboost',
+		[
+			'title'    => esc_html__( 'WCBoost', 'wcboost' ),
+			'priority' => 1000,
+		]
+	);
+
+	$manager->add_setting( [
+		'type'     => 'text',
+		'name'     => 'brevo_conversion_id',
+		'label'    => __( 'Brevo Conversion ID', 'wcboost' ),
+		'default'  => '',
+		'section'  => 'wcboost',
+	] );
+} );
+
+// Brevo chat.
+add_action( 'wp_footer', function() {
+	$brevo_conversion_id = get_theme_mod( 'brevo_conversion_id' );
+
+	if ( ! $brevo_conversion_id ) {
+		return;
+	}
+	//610bb77f3846263c364feb6c
+	?>
+	<!-- Brevo Conversations {literal} -->
+	<script>
+		(function(d, w, c) {
+			w.BrevoConversationsID = '<?php echo esc_js( $brevo_conversion_id ); ?>';
+			w[c] = w[c] || function() {
+				(w[c].q = w[c].q || []).push(arguments);
+			};
+			var s = d.createElement('script');
+			s.async = true;
+			s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+			if (d.head) d.head.appendChild(s);
+		})(document, window, 'BrevoConversations');
+	</script>
+	<!-- /Brevo Conversations {/literal} -->
+	<?php
+} );
 
 include __DIR__ . '/inc/extension.php';
 include __DIR__ . '/inc/docs.php';
